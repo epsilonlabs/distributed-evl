8f4e422a2aeb8e54351fb4d462e720b42aa35c30
Sina Madani <sinadoom@googlemail.com>
2020-04-06 23:19:29 +0100

    Tidied up pom

--- org.eclipse.epsilon.evl.engine.distributed/src/org/eclipse/epsilon/evl/distributed/EvlModuleDistributed.java
+++ org.eclipse.epsilon.evl.engine.distributed/src/org/eclipse/epsilon/evl/distributed/EvlModuleDistributed.java
@@ -24,37 +24,37 @@ import org.eclipse.epsilon.evl.distributed.execute.context.EvlContextDistributed
 public abstract class EvlModuleDistributed extends EvlModuleParallelContextAtoms {
    
    public EvlModuleDistributed(EvlContextDistributed context) {
-      super(Objects.requireNonNull(context));
+	  super(Objects.requireNonNull(context));
    }
    
    @Override
    public EvlContextDistributed getContext() {
-      return (EvlContextDistributed) super.getContext();
+	  return (EvlContextDistributed) super.getContext();
    }
    
    /**
-    * Retrieves the model element at the specified index in the jobs list.
-    * 
-    * @param index The position of the model element in the jobs list. 
-    * @return The model element.
-    * @throws EolRuntimeException
-    */
+	* Retrieves the model element at the specified index in the jobs list.
+	* 
+	* @param index The position of the model element in the jobs list. 
+	* @return The model element.
+	* @throws EolRuntimeException
+	*/
    public Object modelElementAtIndex(int index) throws EolRuntimeException {
-      return getAllJobs().get(index).element;
+	  return getAllJobs().get(index).element;
    }
    
    /**
-    * Finds the position of the specified model element in the jobs list.
-    * 
-    * @param element The model element to find from the jobs list.
-    * @return The index of the model element if present, or a negative value if absent.
-    * @throws EolRuntimeException
-    */
+	* Finds the position of the specified model element in the jobs list.
+	* 
+	* @param element The model element to find from the jobs list.
+	* @return The index of the model element if present, or a negative value if absent.
+	* @throws EolRuntimeException
+	*/
    public int indexOfModelElement(Object element) throws EolRuntimeException {
-      Iterator<? extends RuleAtom<?>> iter = getAllJobs().iterator();
-      for (int i = 0; iter.hasNext(); i++) {
-         if (iter.next().element == element) return i;
-      }
-      return -1;
+	  Iterator<? extends RuleAtom<?>> iter = getAllJobs().iterator();
+	  for (int i = 0; iter.hasNext(); i++) {
+		 if (iter.next().element == element) return i;
+	  }
+	  return -1;
    }
 }
